6cd872e72fb37c95aceb1fce1f9ea46bf17b4d16
JAGPREET SINGH <singhjagpreet096@gmail.com>
2023-12-11 03:43:24 -0500

    removed cache

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,7 +5,7 @@ from steps.clean_data import clean_df
 from steps.model_train import train_model
 from steps.evaluation import model_evaluate
 
-@pipeline 
+@pipeline(enable_cache=True)
 def train_pipeline(data_path: str):
     df = data_ingestion(data_path)
     clean_df(df)
