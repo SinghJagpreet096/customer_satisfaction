de1098901434b2d26b6e1e99164db3e7962e65c2
JAGPREET SINGH <singhjagpreet096@gmail.com>
2023-12-12 04:19:40 -0500

    model training

--- steps/model_train.py
+++ steps/model_train.py
@@ -2,7 +2,28 @@ import logging
 import pandas as pd
 from zenml import step
 
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
+
 @step
-def train_model(df: pd.DataFrame):
+def train_model(
+    X_train: pd.DataFrame,
+    X_test: pd.DataFrame,
+    y_train: pd.Series,
+    y_test: pd.Series,
+    config: ModelNameConfig,
+    ) -> RegressorMixin:
     logging.info(f"training begin")
-    pass
+    try:
+        if config.model_name == "LinearRegression":
+            model = LinearRegressionModel()
+            trained_model = model.train(X_train, y_train)
+            return trained_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+
+        logging.error(f"Error in model_train: {e}")
+        raise e
+    
